d09b96f04073622c0ab085f2e26493e118f18ad7
SenaKebe <152234422+SenaKebe@users.noreply.github.com>
2025-10-21 21:31:27 +0300

    preparation for deployment

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="127.0.0.1",
-        port=8000,
+        port=int(os.getenv("PORT", 7860)),
         reload=True,  # Enable auto-reload during development
         log_level="info"
     )
