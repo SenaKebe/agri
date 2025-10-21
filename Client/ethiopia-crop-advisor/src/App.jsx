105f8df6905e3106178f22eec37530a4ff2b159e
SenaKebe <152234422+SenaKebe@users.noreply.github.com>
2025-10-21 22:31:28 +0300

    link_from_backend

--- Client/ethiopia-crop-advisor/src/App.jsx
+++ Client/ethiopia-crop-advisor/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api/v1";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 function App() {
   const [messages, setMessages] = useState([]);
